3c33ffcf201590c2b90ae1307c17aff038aba274
alux444 <aluux999@gmail.com>
2023-09-07 00:54:30 +1200

    style - Moved buttons to corners

--- src/components/Displays/SongDisplay.tsx
+++ src/components/Displays/SongDisplay.tsx
@@ -114,173 +114,176 @@ const SongDisplay = ({
         <div
             className={`songDisplay md:flex flex-col flex-wrap justify-center w-full items-center h-fit`}
         >
-            <div
-                className={`bg-dark3 hover h-full flex justify-between flex xs:flex-row items-center p-2 w-full border-[1px]
-                 rounded-[10px] border-dark2 backdrop-blur-3xl ${
-                     selected &&
-                     "border-lightgreen border-[2px] bg-[rgba(248,191,255,0.1)]"
-                 }`}
-            >
-                <div className="namesAndImage flex gap-1 w-[80%] h-full items-center">
-                    <div className="imageDiv flex flex-col gap-1 w-fit">
-                        <a
-                            href={songInfo.external_urls.spotify}
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <img
-                                src={songInfo.album.images[1].url}
-                                className="float-left md:max-w-[80px] md:max-h-[80px] max-h-[70px] max-w-[70px]"
+            <div className="flex flex-col w-full">
+                <div className="flex justify-between w-full">
+                    <button
+                        className={`absolute w-fit right-2 top-3 py-[3px] z-50 ${
+                            inCart ? "buttoncancel" : "buttonselect"
+                        }`}
+                        type="button"
+                        onClick={() => {
+                            if (inCart) {
+                                removeFromCart(songInfo);
+                            } else {
+                                addToCart(songInfo);
+                            }
+                        }}
+                    >
+                        <span className="flex justify-center items-center w-full">
+                            <FavoriteBorderOutlined
+                                style={{ fontSize: "1rem" }}
                             />
-                        </a>
-                        <button
-                            className={`${
-                                inCart ? "buttoncancel" : "buttonselect"
-                            } w-full`}
-                            type="button"
-                            onClick={() => {
-                                if (inCart) {
-                                    removeFromCart(songInfo);
-                                } else {
-                                    addToCart(songInfo);
-                                }
-                            }}
-                        >
-                            <span className="flex justify-center items-center w-full">
-                                <FavoriteBorderOutlined
-                                    style={{ fontSize: "1rem" }}
-                                />
-                            </span>
-                        </button>
-                    </div>
-
-                    <div className="namesDiv flex justify-between gap-1 p-1 flex-col h-full flex-grow max-w-[70%] sm:max-w-[70%] lg:max-w-[80%]">
-                        <div className="flex flex-col w-full">
-                            <h2 className="text-md flex gap-2 w-full overflow-hidden min-w-0 mb-1">
-                                <span className="truncate max-w-full">
-                                    {songInfo.name}
-                                </span>
-                            </h2>
-                            <div className="text-xs text-slate-400 flex gap-2 flex-wrap w-full overflow-hidden">
-                                <span className="truncate max-w-full">
-                                    {songInfo.artists.length > 3
-                                        ? artists +
-                                          " +" +
-                                          (songInfo.artists.length - 3)
-                                        : artists}
-                                </span>
-                            </div>
-                        </div>
-
-                        <div className="text-slate-400">
-                            {duration && (
-                                <>
-                                    <span>{duration}</span>{" "}
-                                    <span>
-                                        {" "}
-                                        ·{" "}
-                                        {songInfo.album.release_date.slice(
-                                            0,
-                                            4
-                                        )}
-                                    </span>
-                                </>
-                            )}
-                        </div>
-                    </div>
-                </div>
-
-                <div className="buttonsArea flex flex-col align-center flex-wrap gap-1 justify-end items-end w-[15%] xs:w-[20%]">
+                        </span>
+                    </button>
                     {!selected && (
                         <button
                             id="songAddButton"
-                            className="buttonselect w-full"
+                            className="buttonselect w-fit absolute left-2 top-3 py-[3px] z-50"
                             onClick={() => {
                                 addSong(songInfo);
                             }}
                         >
-                            <span className="flex justify-center xs:justify-start items-center w-full">
+                            <span className="flex justify-center xs:justify-start items-center w-fit">
                                 <AddIcon style={{ fontSize: "0.9rem" }} />
-                                <span className="hidden xs:flex">ADD</span>
                             </span>
                         </button>
-                    )}{" "}
+                    )}
                     {selected && (
                         <button
                             id="songRemoveButton"
-                            className="buttoncancel w-full"
+                            className="buttoncancel absolute w-fit left-2 top-3 py-[3px] z-50"
                             onClick={() => {
                                 removeSong(songInfo);
                             }}
                         >
                             <span className="flex justify-center xs:justify-start items-center w-full">
                                 <ClearIcon style={{ fontSize: "0.9rem" }} />
-                                <span className="hidden xs:flex">REMOVE</span>
-                            </span>
-                        </button>
-                    )}
-                    {songInfo.preview_url && (
-                        <button
-                            className="buttonprev w-full"
-                            type="button"
-                            onClick={playPreview}
-                        >
-                            <span className="w-full">
-                                {audioIsPlaying &&
-                                currentPlayingId === songInfo.id ? (
-                                    <div className="flex justify-center xs:justify-start items-center w-full">
-                                        <PauseIcon
-                                            style={{ fontSize: "0.9rem" }}
-                                        />
-                                        <span className="hidden xs:flex">
-                                            PAUSE
-                                        </span>
-                                    </div>
-                                ) : (
-                                    <div className="flex justify-center xs:justify-start items-center w-full">
-                                        <PlayArrowIcon
-                                            style={{ fontSize: "0.9rem" }}
-                                        />
-                                        <span className="hidden xs:flex">
-                                            PLAY
-                                        </span>
-                                    </div>
-                                )}
-                            </span>
-                        </button>
-                    )}
-                    {statsButton && (
-                        <button
-                            className="buttonprev w-full"
-                            type="button"
-                            onClick={() => setThisShowStats(!thisShowStats)}
-                        >
-                            <span className="flex justify-center xs:justify-start items-center w-full">
-                                {thisShowStats ? (
-                                    <span>
-                                        <VisibilityOffIcon
-                                            style={{ fontSize: "1rem" }}
-                                        />
-                                    </span>
-                                ) : (
-                                    <BarChartIcon
-                                        style={{ fontSize: "1rem" }}
-                                    />
-                                )}
-                                <span className="hidden xs:flex">STATS</span>
                             </span>
                         </button>
                     )}
                 </div>
-            </div>
+                <div
+                    className={`bg-dark3 hover h-full flex justify-between flex xs:flex-row items-center p-2 w-full border-[1px]
+                 rounded-[10px] border-dark2 backdrop-blur-3xl ${
+                     selected &&
+                     "border-lightgreen border-[2px] bg-[rgba(248,191,255,0.1)]"
+                 }`}
+                >
+                    <div className="namesAndImage flex gap-1 w-[80%] h-full items-center">
+                        <div className="imageDiv flex flex-col gap-1 w-fit">
+                            <a
+                                href={songInfo.external_urls.spotify}
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <img
+                                    src={songInfo.album.images[1].url}
+                                    className="float-left md:max-w-[80px] md:max-h-[80px] max-h-[70px] max-w-[70px]"
+                                />
+                            </a>
+                        </div>
 
-            {features && thisShowStats && (
-                <FeaturesDisplay
-                    features={features}
-                    onClose={() => setThisShowStats(false)}
-                    songInfo={songInfo}
-                />
-            )}
+                        <div className="namesDiv flex justify-between gap-1 p-1 flex-col h-full flex-grow max-w-[70%] sm:max-w-[70%] lg:max-w-[80%]">
+                            <div className="flex flex-col w-full">
+                                <h2 className="text-md flex gap-2 w-full overflow-hidden min-w-0 mb-1">
+                                    <span className="truncate max-w-full">
+                                        {songInfo.name}
+                                    </span>
+                                </h2>
+                                <div className="text-xs text-slate-400 flex gap-2 flex-wrap w-full overflow-hidden">
+                                    <span className="truncate max-w-full">
+                                        {songInfo.artists.length > 3
+                                            ? artists +
+                                              " +" +
+                                              (songInfo.artists.length - 3)
+                                            : artists}
+                                    </span>
+                                </div>
+                            </div>
+
+                            <div className="text-slate-400">
+                                {duration && (
+                                    <>
+                                        <span>{duration}</span>{" "}
+                                        <span>
+                                            {" "}
+                                            ·{" "}
+                                            {songInfo.album.release_date.slice(
+                                                0,
+                                                4
+                                            )}
+                                        </span>
+                                    </>
+                                )}
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="buttonsArea flex flex-col align-center flex-wrap gap-1 justify-end items-end w-[15%] xs:w-[20%]">
+                        {songInfo.preview_url && (
+                            <button
+                                className="buttonprev w-full"
+                                type="button"
+                                onClick={playPreview}
+                            >
+                                <span className="w-full">
+                                    {audioIsPlaying &&
+                                    currentPlayingId === songInfo.id ? (
+                                        <div className="flex justify-center xs:justify-start items-center w-full">
+                                            <PauseIcon
+                                                style={{ fontSize: "0.9rem" }}
+                                            />
+                                            <span className="hidden xs:flex">
+                                                PAUSE
+                                            </span>
+                                        </div>
+                                    ) : (
+                                        <div className="flex justify-center xs:justify-start items-center w-full">
+                                            <PlayArrowIcon
+                                                style={{ fontSize: "0.9rem" }}
+                                            />
+                                            <span className="hidden xs:flex">
+                                                PLAY
+                                            </span>
+                                        </div>
+                                    )}
+                                </span>
+                            </button>
+                        )}
+                        {statsButton && (
+                            <button
+                                className="buttonprev w-full"
+                                type="button"
+                                onClick={() => setThisShowStats(!thisShowStats)}
+                            >
+                                <span className="flex justify-center xs:justify-start items-center w-full">
+                                    {thisShowStats ? (
+                                        <span>
+                                            <VisibilityOffIcon
+                                                style={{ fontSize: "1rem" }}
+                                            />
+                                        </span>
+                                    ) : (
+                                        <BarChartIcon
+                                            style={{ fontSize: "1rem" }}
+                                        />
+                                    )}
+                                    <span className="hidden xs:flex">
+                                        STATS
+                                    </span>
+                                </span>
+                            </button>
+                        )}
+                    </div>
+                </div>
+                {features && thisShowStats && (
+                    <FeaturesDisplay
+                        features={features}
+                        onClose={() => setThisShowStats(false)}
+                        songInfo={songInfo}
+                    />
+                )}
+            </div>
         </div>
     );
 };
