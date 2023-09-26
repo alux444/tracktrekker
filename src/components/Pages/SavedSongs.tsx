58f1322e9c26596c4255c97cf1bd072900f922e7
alux444 <aluux999@gmail.com>
2023-09-26 13:16:54 +1300

    bug - Fixed bug with old titles and modules imported

---
+++ src/components/Pages/SavedSongs.tsx
@@ -0,0 +1,70 @@
+import { useContext, useEffect, useRef, useState } from "react";
+import useOutsideClick from "../../utils/useOutsideClose";
+import { DevContext } from "../../App";
+import usePlaylist from "../../utils/usePlaylist";
+import SavedSongDisplay from "../Displays/SavedSongDisplay";
+
+const SavedSongs = ({ onClose }: { onClose: () => void }) => {
+    const { devMode, savedSongs, setSavedSongs } = useContext(DevContext);
+    const [saved, setSaved] = useState<boolean>(false);
+
+    const modalRef = useRef(null);
+    const { createPlaylist } = usePlaylist();
+    useOutsideClick(modalRef, onClose);
+
+    useEffect(() => {
+        setSaved(false);
+    }, [savedSongs]);
+
+    const cart = savedSongs.map((song) => (
+        <SavedSongDisplay songInfo={song} key={song.id} />
+    ));
+
+    return (
+        <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-black bg-opacity-50 z-50 w-screen h-screen">
+            <div
+                ref={modalRef}
+                className="relative bg-dark3 flex flex-col p-5 gap-1 rounded-lg shadow-md flex items-center max-h-[90vh] w-[90vw] md:w-[70vw] lg:w-[60vw] xl:w-[50vw] overflow-auto"
+            >
+                <button
+                    className="absolute top-2 right-3 cursor-pointer"
+                    onClick={onClose}
+                >
+                    <span>&times;</span>
+                </button>
+                <p className="grad text-lg">Saved Songs</p>
+                <p>Click the heart button to save songs!</p>
+                <p className="">
+                    {savedSongs.length}{" "}
+                    {savedSongs.length == 1 ? "Song" : "Songs"} Saved
+                </p>
+                {savedSongs.length > 0 && (
+                    <button
+                        className="buttoncancel"
+                        onClick={() => setSavedSongs([])}
+                    >
+                        <span>Clear Songs</span>
+                    </button>
+                )}
+                {cart}
+                {savedSongs.length > 0 &&
+                    devMode &&
+                    (saved ? (
+                        <p className="grad">Saved to your Spotify!</p>
+                    ) : (
+                        <button
+                            className="grad button2"
+                            onClick={() => {
+                                createPlaylist(savedSongs);
+                                setSaved(true);
+                            }}
+                        >
+                            Create a Playlist
+                        </button>
+                    ))}
+            </div>
+        </div>
+    );
+};
+
+export default SavedSongs;
